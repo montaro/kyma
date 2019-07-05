289a9cf8f622bc5cd110108899731e0bc4867f31
Sayan Hazra <sayan.mnit@gmail.com>
2019-07-05 15:20:14 +0200

    Removed commented code

--- components/event-bus/test/util/v2/publish.go
+++ components/event-bus/test/util/v2/publish.go
@@ -2,15 +2,11 @@ package util
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
-
-	api "github.com/kyma-project/kyma/components/event-bus/api/publish"
-	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -178,116 +174,3 @@ func PerformPublishV2RequestWithHeaders(t *testing.T, publishURL string, payload
 
 	return body, res.StatusCode
 }
-
-// AssertExpectedError asserts an expected status-code and error.
-func AssertExpectedError(t *testing.T, body []byte, actualStatusCode int, expectedStatusCode int, errorField interface{}, errorType interface{}) {
-	var responseError api.Error
-	err := json.Unmarshal(body, &responseError)
-	assert.Equal(t, expectedStatusCode, actualStatusCode)
-	assert.Nil(t, err)
-	if errorType != nil {
-		assert.Equal(t, errorType, responseError.Type)
-	}
-	if errorField != nil {
-		assert.NotNil(t, responseError.Details)
-		assert.NotEqual(t, len(responseError.Details), 0)
-		assert.Equal(t, errorField, responseError.Details[0].Field)
-	}
-}
-
-// // BuildDefaultTestPayload returns a default test payload.
-// func BuildDefaultTestPayload() string {
-// 	return BuildTestPayload(TestSourceID, TestEventType, TestEventTypeVersion, TestEventID, TestEventTime, TestData)
-// }
-
-// // BuildDefaultTestBadPayload returns a default test bad payload.
-// func BuildDefaultTestBadPayload() string {
-// 	builder := new(eventBuilder).
-// 		build(sourceIDFormat, TestSourceID).
-// 		build(eventTypeFormat, TestEventType).
-// 		build(eventTypeVersionFormat, TestEventTypeVersion).
-// 		build(eventIDFormat, TestEventID).
-// 		build(eventTimeFormat, TestEventTime).
-// 		build(dataFormat, TestData)
-// 	_, _ = builder.WriteString(",") // spoil the payload
-// 	payload := builder.String()
-// 	return payload
-// }
-
-// // BuildDefaultTestPayloadWithoutEventType returns a default test payload without the event-type.
-// func BuildDefaultTestPayloadWithoutEventType() string {
-// 	builder := new(eventBuilder).
-// 		build(sourceIDFormat, TestSourceID).
-// 		build(eventTypeVersionFormat, TestEventTypeVersion).
-// 		build(eventIDFormat, TestEventID).
-// 		build(eventTimeFormat, TestEventTime).
-// 		build(dataFormat, TestData)
-// 	payload := builder.String()
-// 	return payload
-// }
-
-// // BuildDefaultTestPayloadWithoutEventTypeVersion returns a default test payload without the event-type-version.
-// func BuildDefaultTestPayloadWithoutEventTypeVersion() string {
-// 	builder := new(eventBuilder).
-// 		build(sourceIDFormat, TestSourceID).
-// 		build(eventTypeFormat, TestEventType).
-// 		build(eventIDFormat, TestEventID).
-// 		build(eventTimeFormat, TestEventTime).
-// 		build(dataFormat, TestData)
-// 	payload := builder.String()
-// 	return payload
-// }
-
-// // BuildDefaultTestPayloadWithoutEventTime returns a default test payload without the event-time
-// func BuildDefaultTestPayloadWithoutEventTime() string {
-// 	builder := new(eventBuilder).
-// 		build(sourceIDFormat, TestSourceID).
-// 		build(eventTypeFormat, TestEventType).
-// 		build(eventTypeVersionFormat, TestEventTypeVersion).
-// 		build(eventIDFormat, TestEventID).
-// 		build(dataFormat, TestData)
-// 	payload := builder.String()
-// 	return payload
-// }
-
-// // BuildDefaultTestPayloadWithoutData returns a default test payload without the data
-// func BuildDefaultTestPayloadWithoutData() string {
-// 	builder := new(eventBuilder).
-// 		build(sourceIDFormat, TestSourceID).
-// 		build(eventTypeFormat, TestEventType).
-// 		build(eventTypeVersionFormat, TestEventTypeVersion).
-// 		build(eventIDFormat, TestEventID).
-// 		build(eventTimeFormat, TestEventTime)
-// 	payload := builder.String()
-// 	return payload
-// }
-
-// // BuildDefaultTestPayloadWithEmptyData returns a default test payload with empty data
-// func BuildDefaultTestPayloadWithEmptyData() string {
-// 	builder := new(eventBuilder).
-// 		build(sourceIDFormat, TestSourceID).
-// 		build(eventTypeFormat, TestEventType).
-// 		build(eventTypeVersionFormat, TestEventTypeVersion).
-// 		build(eventIDFormat, TestEventID).
-// 		build(eventTimeFormat, TestEventTime).
-// 		build(dataFormat, TestDataEmpty)
-// 	payload := builder.String()
-// 	return payload
-// }
-
-// // PerformPublishV1Request performs a test publish request.
-// func PerformPublishV1Request(t *testing.T, publishURL string, payload string) ([]byte, int) {
-// 	res, err := http.Post(publishURL+"/v1/events", "application/json", strings.NewReader(payload))
-
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	defer func() { _ = res.Body.Close() }()
-// 	body, err := ioutil.ReadAll(res.Body)
-
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	return body, res.StatusCode
-// }
